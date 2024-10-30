2d61970b600fb35c8091acd1f737809bf9df0b7b
caioescorcio <caio.dourado@usp.br>
2024-10-30 13:40:59 -0300

    final file

--- ativ3/float_lib.c
+++ ativ3/float_lib.c
@@ -1,23 +1,16 @@
+/*
+float_lib: Atividade 3 de OAC
+
+Caio Escorcio Lima Dourado - 13680313
+30/10/2024
+
+
+ */
+
 #include "float_lib.h"
 #define FILTER_EXP 0x7F800000
 #define FILTER_SIGN 0x80000000
 #define FILTER_FRACT 0x7FFFFF
-#define FILTER_32 0xFFFFFF
-#define ROUND_BIT 0x00000040
-
-void printBinary(int num)
-{
-    // O tamanho em bits de um inteiro
-    int bits = sizeof(num) * 8;
-
-    for (int i = bits - 1; i >= 0; i--)
-    {
-        // Cria um bit a partir da posição atual
-        int bit = (num >> i) & 1;
-        printf("%d", bit);
-    }
-    printf("\n");
-}
 
 mfloat floatsisf(mint i)
 {
@@ -50,7 +43,7 @@ mint fixsfsi(mfloat a)
     if (a == (mfloat)0)
         return (mint)0;
     mint r = a;
-    mint exp = (a & FILTER_EXP) >> 23;
+    mint exp = (a & FILTER_EXP) >> 23; // acha o expoente
     r &= FILTER_FRACT;
     r |= (1 << 23);
     exp -= 127;
@@ -62,7 +55,7 @@ mint fixsfsi(mfloat a)
     }
     else
     {
-        r >>= (23 - exp);
+        r >>= (23 - exp); // faz o shift para a mantissa ficar com 23 bits
     }
     if ((a & FILTER_SIGN) != 0)
         r *= -1;
@@ -71,7 +64,7 @@ mint fixsfsi(mfloat a)
 
 mfloat negsf2(mfloat a)
 {
-    mfloat neg = a ^ FILTER_SIGN;
+    mfloat neg = a ^ FILTER_SIGN; // bit-shift
 
     return neg;
 }
@@ -83,6 +76,7 @@ mfloat addsf3(mfloat a, mfloat b)
     if ((a == 0x00000000))
         return b;
 
+    // filtra os dados de cada um dos operandos para analisar quais os expoentes, fracoes e sinais
     mfloat exp_a = (a & FILTER_EXP) >> 23;
     mfloat exp_b = (b & FILTER_EXP) >> 23;
     mfloat fract_a = (a & FILTER_FRACT) | (1 << 23);
@@ -91,10 +85,10 @@ mfloat addsf3(mfloat a, mfloat b)
     mfloat sign_b = (b & FILTER_SIGN) >> 31;
     mfloat fract, exp, sign;
 
-    if (exp_a >= exp_b)
+    if (exp_a >= exp_b) // acha qual sinal eh maior
     {
-        fract_b = (exp_a - exp_b) < 32 ? fract_b >> (exp_a - exp_b) : 0;
-        exp = exp_a;
+        fract_b = (exp_a - exp_b) < 32 ? fract_b >> (exp_a - exp_b) : 0; // evita underflow para caso a diferenca de expoentes sejam grandes
+        exp = exp_a;                                                     // configura o expoente
     }
     else
     {
@@ -102,19 +96,19 @@ mfloat addsf3(mfloat a, mfloat b)
         exp = exp_b;
     }
 
-    if (sign_a == sign_b)
+    if (sign_a == sign_b) // analisa os sinais
     {
         fract = fract_a + fract_b;
-        sign = sign_a;
+        sign = sign_a; // seta o sinal
     }
     else
     {
-        fract = (fract_a >= fract_b) ? fract_a - fract_b : fract_b - fract_a;
+        fract = (fract_a >= fract_b) ? fract_a - fract_b : fract_b - fract_a; // casos de subtracao
         sign = (fract_a >= fract_b) ? sign_a : sign_b;
         if (fract == 0)
             return 0;
     }
-    while ((fract & (1 << 24)))
+    while ((fract & (1 << 24))) // reajuste do expoende e da mantissa
     {
         fract >>= 1;
         exp++;
@@ -131,7 +125,7 @@ mfloat addsf3(mfloat a, mfloat b)
     return (sign << 31) | (exp << 23) | fract;
 }
 
-mfloat subsf3(mfloat a, mfloat b)
+mfloat subsf3(mfloat a, mfloat b) // subtracao eh uma soma com um numero negativo
 {
     return addsf3(a, negsf2(b));
 }
--- ativ3/float_lib.h
+++ ativ3/float_lib.h
@@ -1,13 +1,4 @@
 #include <inttypes.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdint.h>
-
-#define FILTER_EXP 0x7F800000
-#define FILTER_SIGN 0x80000000
-#define FILTER_FRACT 0x7FFFFF
-#define FILTER_32 0xFFFFFF
 
 typedef int32_t mint;
 typedef uint32_t mfloat;
@@ -17,4 +8,3 @@ mint fixsfsi(mfloat a);
 mfloat negsf2(mfloat a);
 mfloat addsf3(mfloat a, mfloat b);
 mfloat subsf3(mfloat a, mfloat b);
-void printBinary(int num);
