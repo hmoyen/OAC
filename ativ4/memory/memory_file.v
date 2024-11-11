53fc64ac126a2d416c913b64534db31daaf56f36
caioescorcio <caioeldourado@gmail.com>
2024-11-11 11:51:04 -0300

    memory files unstarted

---
+++ ativ4/memory/memory_file.v
@@ -0,0 +1,28 @@
+module memory_file(
+    input   [31:0]  addr,       // address
+    input   [31:0]  Din,        // data input
+    input           we,         // write enable
+    input           re,         // read enable
+    input           clk,
+    output  [31:0]  out  
+);
+
+    integer i;
+    parameter size = 256;
+
+    reg [size - 1:0][31:0] memory;   // memory = 256 positions array for 32 bits 
+
+    initial begin                   // starting memory positions
+        for(i = 0; i < size; i = i + 1) begin
+            memory[i] = 32'b0; 
+        end      
+    end
+
+    always@(posedge clk) begin
+        if(we == 1'b1) begin
+            memory[address] <= Din;
+        end
+    end
+
+assign out = re ? memory[addr] : out;
+endmodule
